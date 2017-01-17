6d5e949613f77cd7b298a84c9024743628fff1e2
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-01-17 11:17:38 +0530

    Downloading Image from web

--- Downloading Images From The Web/ViewController.swift
+++ Downloading Images From The Web/ViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Loaded")
+        //Steps
+        
+        //1 Create an Url
+        let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Johann_Sebastian_Bach.jpg/260px-Johann_Sebastian_Bach.jpg")!
+        
+        //2 Create a request
+        let request = NSMutableURLRequest(url: url)
+        
+        //3 Create a task
+        let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {
+            
+            data, response, error in
+            
+            if error != nil {
+                
+                print("error occured while downloading data")
+                
+            } else {
+                
+                if let data = data {
+                    
+                    if let image = UIImage(data: data) {
+                        
+                        self.imageView.image = image;
+                        
+                    }
+                }
+                
+            }
+            
+        })
+        
+        task.resume()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
